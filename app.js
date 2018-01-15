a72a8a0baba6280d479a02eb3e8e320191637fe0
anna_umi <anna.a.vlasenko@yandex.ua>
2018-01-15 22:41:40 +0200

    change port and del pass

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(function (err, req, res, next) { // если будет ошибка н�
 });
 
 // запускаю сервер
-server.listen(3000, 'localhost');
+server.listen(80, '92.53.105.224');
 server.on('listening', function () {
    if (!fs.existsSync(uploadDir)) { // если нет мойей папки upload то сосздать
     fs.mkdirSync(uploadDir);
